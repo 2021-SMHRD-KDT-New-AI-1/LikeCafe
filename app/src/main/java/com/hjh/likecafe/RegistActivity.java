566406e5894fa17c073ba09202380738ebff485b
HamGildo <ham5477@naver.com>
2021-10-12 23:13:17 +0900

    Node.js 서버로 Json보내는 코드 작성

--- app/src/main/java/com/hjh/likecafe/RegistActivity.java
+++ app/src/main/java/com/hjh/likecafe/RegistActivity.java
@@ -1,14 +1,85 @@
 package com.hjh.likecafe;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistActivity extends AppCompatActivity {
+    RequestQueue requestQueue;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+
+        button3 = findViewById(R.id.button3);
+
+        // 서버와 통신
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postJson();
+            }
+        });
     }
+
+    public void postJson() {
+        String url = "http://172.30.1.8:3003/MemberRegist";
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("mem_id", "test");
+                params.put("pw", "1234");
+                params.put("nick", "tt");
+                params.put("birth", "1998-05-04");
+                params.put("gender", "여자");
+
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
+
 }
