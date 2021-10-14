45c79ee8b18aab0b4d434cfdc123c09b0e9d4efd
namu1005 <3690cm@naver.com>
2021-10-14 11:24:01 +0900

    로그인 완료 페이지 완료

--- app/src/main/java/com/hjh/likecafe/loginsucc.java
+++ app/src/main/java/com/hjh/likecafe/loginsucc.java
@@ -2,13 +2,28 @@ package com.hjh.likecafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginsucc extends AppCompatActivity {
+    Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginsucc);
+
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
