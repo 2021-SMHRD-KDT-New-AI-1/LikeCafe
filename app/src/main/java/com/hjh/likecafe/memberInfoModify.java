d96b71f4d408173dfceddaa931053abd57b11579
65646269 <b65646269@gmail.com>
2021-10-15 12:05:16 +0900

    회원정보수정(프사선택 및 프사변경2)

--- app/src/main/java/com/hjh/likecafe/memberInfoModify.java
+++ app/src/main/java/com/hjh/likecafe/memberInfoModify.java
@@ -1,16 +1,21 @@
 package com.hjh.likecafe;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -28,6 +33,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,12 +46,16 @@ public class memberInfoModify extends AppCompatActivity {
     EditText et_changeNick, et_currentPw, et_changePw, et_chkPw;
     RadioGroup rg_sex;
     RadioButton rb_female, rb_male;
-    Button btn_modify;
+    Button btn_modify, btn_picChange;
     String sex;
+    boolean modifyStatus;
+
+    ImageView img_profile;
 
     // '생년월일'
     private TextView textView_Date;
     private DatePickerDialog.OnDateSetListener callbackMethod;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +70,20 @@ public class memberInfoModify extends AppCompatActivity {
         rb_female = findViewById(R.id.rb_female);
         rb_male = findViewById(R.id.rb_male);
         btn_modify = findViewById(R.id.btn_modify);
+        btn_picChange = findViewById(R.id.btn_picChange);
+
+
+
+        // '사진수정'버튼 클릭 시 폰 갤러리 열기
+        btn_picChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,1);
+            }
+        });
 
 
         // 라디오 체크 리스너
@@ -84,14 +110,14 @@ public class memberInfoModify extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String nick = et_changeNick.getText().toString();
-                String currentPw = et_currentPw.getText().toString();
-                String changePw = et_changePw.getText().toString();
+                String pw = et_changePw.getText().toString();
                 String birth = textView_Date.getText().toString();
 
                 // 입력값 검사 부분 (시간 남을 시 닉네임 중복검사, 빈칸 검사 코드 추가)
-                boolean chkPw = changePw.equals(et_chkPw.getText().toString());
+                boolean chkPw = pw.equals(et_chkPw.getText().toString());
                 boolean chkDuplicateNick = true;
                 boolean chkBlank = true;
+                // (보류)boolean chkcurrentPw = this.chkcurrentPw(id, pw);
 
                 if (!chkPw) { // 만약 변경비번과 확인비번의 값이 다른경우
                     Toast.makeText(memberInfoModify.this,
@@ -102,9 +128,9 @@ public class memberInfoModify extends AppCompatActivity {
                 } else if (!chkBlank) { // 빈칸검사?
                     Toast.makeText(memberInfoModify.this,
                             "모든 입력란에 입력을 완료해주세요", Toast.LENGTH_SHORT).show();
-                } else {
-                    // id와 입력받은 값을 매개변수로 하여 modify 메소드 호출
-                    modify("test", currentPw, nick, changePw, birth, sex); // 일단 임시로 id값 대신 test를 넣어주었음
+                } // (보류) else if (!chkcurrentPw) {토스트'비밀번호가 틀렸습니다.'}
+                else {
+                    postModify(nick, pw, birth, sex);
                 }
             }
         });
@@ -113,11 +139,39 @@ public class memberInfoModify extends AppCompatActivity {
         this.InitializeListener(); // '생년월일'
     }
 
+    //선택한 사진으로 프사바꾸기
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        img_profile = findViewById(R.id.img_profile);
+
+        if (requestCode == 1) {
+
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    img_profile.setImageBitmap(img);
+                    Toast.makeText(memberInfoModify.this,
+                            "프로필 사진이 변경되었다능!", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    // '생년월일'
     private void InitializeView() { // '생년월일'
         textView_Date = (TextView) findViewById(R.id.textView_Date);
     }
 
-    private void InitializeListener() { // '생년월일'
+    // '생년월일'
+    private void InitializeListener() {
         callbackMethod = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int monthOfYear
@@ -127,7 +181,8 @@ public class memberInfoModify extends AppCompatActivity {
         };
     }
 
-    public void OnClickHandler(View view) // '생년월일'
+    // '생년월일'
+    public void OnClickHandler(View view)
     {
         DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 1990, 7, 1);
         dialog.show();
@@ -135,7 +190,7 @@ public class memberInfoModify extends AppCompatActivity {
 
     // Json파일을 만들어 웹 서버로 보내기
     public void postModify(String nick, String pw, String birth, String sex) {
-        String url = "http://172.30.1.8:3003/Member/Modify";
+        String url = "http://172.30.1.12:3003/Member/Modify";
         StringRequest request = new StringRequest(
                 Request.Method.POST,
                 url,
@@ -145,6 +200,7 @@ public class memberInfoModify extends AppCompatActivity {
                         try {
                             JSONObject jsonObject = (JSONObject) (new JSONArray(response).get(0));
                             Log.d("status : ", jsonObject.getString("status"));
+                            modifyStatus = true;
                             Intent intent = new Intent
                                     (memberInfoModify.this, MainActivity.class);
                             startActivity(intent);
@@ -174,51 +230,13 @@ public class memberInfoModify extends AppCompatActivity {
         requestQueue.add(request);
     }
 
-    public void modify(String id, String currentPw, String nick, String changePw, String birth, String sex) {
-        // 먼저 현재 비밀번호가 일치하는지부터 확인해야함
-        // 비밀번호랑 id가 매치되는지(올바른 현재 비밀번호를 입력했는지)는 로그인 요청으로 확인가능
-        String url = "http://172.30.1.8:3003/Member/Login"; // 로그인 요청 : id와 pw가 회원 테이블에 있는지 확인해줌
-        // 로그인 요청을 하면 서버에서 로그인 성공여부에 따라 status에 success 혹은 fail을 담아서 보내줌
-        StringRequest request = new StringRequest(
-                Request.Method.POST,
-                url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = (JSONObject) (new JSONArray(response).get(0));
-                            Log.d("status : ", jsonObject.getString("status"));
-                            String status = jsonObject.getString("status");
-
-                            if (status.equals("success")) { // 로그인 성공 (비밀번호가 일치함)
-                                postModify(nick, changePw, birth, sex); // postModify 메소드 호출
-                            } else { // 로그인 실패 (비밀번호가 일치하지 않음)
-                                Toast.makeText(memberInfoModify.this, "현재 비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show();
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("id", id);
-                params.put("pw", currentPw);
-
-                return params;
-            }
-        };
-        requestQueue.add(request);
+    public boolean chkcurrentPw(String id, String pw) {
+        // 서버에 id를 보내서 서버로부터 pw를 받아온다.
+        // 받아온 pw와 매개변수 pw를 비교하여 같으면 t 아니면 f 리턴
+        return true;
     }
+
+
 
 
 }
