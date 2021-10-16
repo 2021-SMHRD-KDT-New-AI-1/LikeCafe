354ca393390bb1fa863105e5b1c11ce6a2f83de3
namu1005 <3690cm@naver.com>
2021-10-16 10:40:54 +0900

    zzimlist Navigation Drawer 추가

--- app/src/main/java/com/hjh/likecafe/zzimlist.java
+++ app/src/main/java/com/hjh/likecafe/zzimlist.java
@@ -1,12 +1,18 @@
 package com.hjh.likecafe;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,9 +21,10 @@ import java.util.Map;
 
 public class zzimlist extends AppCompatActivity {
     ListView lv_zzim;
-
     RequestQueue requestQueue;
     List<CafeVO> data;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,10 @@ public class zzimlist extends AppCompatActivity {
 
         lv_zzim = findViewById(R.id.lv_zzim);
         data = new ArrayList<CafeVO>();
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
 
         CafeAdapter adapter = new CafeAdapter(getApplicationContext(), R.layout.cafelist, data);
         lv_zzim.setAdapter(adapter);
@@ -48,5 +59,42 @@ public class zzimlist extends AppCompatActivity {
 
         adapter.notifyDataSetChanged();
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menu);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.home){
+                }
+                else if(id == R.id.wishlist){
+                }
+                else if(id == R.id.review){
+                }
+                else if(id == R.id.edit){
+                }
+                return true;
+            }
+        });
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 뒤로가기 버튼 눌렀을 때
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
