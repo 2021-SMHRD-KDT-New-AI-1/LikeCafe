e56a705f2be7c535bbe746582ac7edb75ef469fe
HamGildo <ham5477@naver.com>
2021-10-18 02:41:36 +0900

    리뷰 뷰홀더 생성

---
+++ app/src/main/java/com/hjh/likecafe/ReviewViewHolder.java
@@ -0,0 +1,11 @@
+package com.hjh.likecafe;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ReviewViewHolder {
+
+    ImageView img_reviewListImg;
+    ImageView[] stars;
+    TextView tv_reviewListName, tv_reviewListContent;
+}
