ba55018a729b0e461c3f2dbf872c9fd4e5169d45
HamGildo <ham5477@naver.com>
2021-10-19 05:57:49 +0900

    카페리스트> 하트 누르면 찜 목록에 카페를 추가하는 기능 구현

--- app/src/main/java/com/hjh/likecafe/CafeAdapter.java
+++ app/src/main/java/com/hjh/likecafe/CafeAdapter.java
@@ -1,13 +1,28 @@
 package com.hjh.likecafe;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CafeAdapter extends BaseAdapter {
     Context context; // 현재 페이지 정보
@@ -16,6 +31,7 @@ public class CafeAdapter extends BaseAdapter {
 
     private LayoutInflater inflater; // xml파일을 View객체로 변환
     CafeViewHolder viewHolder; // ViewHolder 패턴을 활용하기 위한 변수
+    private RequestQueue requestQueue;
 
     public CafeAdapter(Context context, int layout, List<CafeVO> data) {
         this.context = context;
@@ -42,6 +58,10 @@ public class CafeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context);
+        }
 
         if(view == null) {
             // xml파일(layout)을 view객체로 변환시켜 view에 저장한다.
@@ -73,12 +93,12 @@ public class CafeAdapter extends BaseAdapter {
             public void onClick(View view) {
                 if(data.get(i).isZzimSel()) {
                     // 찜 목록에서 삭제하는 기능
+
                     Toast.makeText(context.getApplicationContext(), "찜 삭제!", Toast.LENGTH_SHORT).show();
                 } else {
                     // 찜 목록에 추가하는 기능
-                    Toast.makeText(context.getApplicationContext(), "찜 추가!", Toast.LENGTH_SHORT).show();
+                    zzimInsert("test", data.get(i).getId(), i);
                 }
-                data.get(i).setZzimSel(!data.get(i).isZzimSel());
                 notifyDataSetChanged();
             }
         });
@@ -90,5 +110,46 @@ public class CafeAdapter extends BaseAdapter {
         });
 
         return view;
+    }
+
+    public void zzimInsert(String mem_id, int cafe_id, int i) {
+        String url = "http://172.30.1.8:3003/Zzim/Insert";
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // 응답 성공 응답 성공 이야후~~!!
+                        try {
+                            JSONObject jsonObject = (JSONObject) (new JSONArray(response).get(0));
+                            String status = jsonObject.getString("status");
+                            Toast.makeText(context.getApplicationContext(), "찜 추가!", Toast.LENGTH_SHORT).show();
+                            data.get(i).setZzimSel(true);
+                            data.get(i).setZzimCnt(data.get(i).getZzimCnt() + 1);
+                            notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("cafe_id", String.valueOf(cafe_id));
+                params.put("mem_id", mem_id);
+
+                return params;
+            }
+        };
+        requestQueue.add(request);
     }
 }
